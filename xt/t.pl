3fd84709752a57327ac247f8d92d14fc7fe32a0c
Steffen Mueller <smueller@cpan.org>
2011-05-29 19:34:05 +0200

    more manual testing

--- xt/t.pl
+++ xt/t.pl
@@ -3,26 +3,47 @@ use strict;
 use warnings;
 use Test::More;
 use Algorithm::SpatialIndex::Bucket::XS;
+use Data::Dumper;
+$|=1;
+SCOPE: {
+  my $b =Algorithm::SpatialIndex::Bucket::XS->new(
+    items => [
+      [17,5,5],
+    ],
+    node_id => 123,
+  );
+  $b->dump;
+  $b->add_items([1,2,3], [2,3,4], [9,9,9]);
+  $b->dump;
+
+  #$b->items();
+  #$b->dump;
+
+  $b->add_items([3..5], [4..6]);
+  $b->dump;
+  $b->add_items([5..7]);
+
+  warn Dumper $b->items;
+}
 
 my $b =Algorithm::SpatialIndex::Bucket::XS->new(
   items => [
-  [1,1,1],
-  [2,3,4],
-  [3,1,1],
-  [4,1,1],
   ],
   node_id => 123,
 );
+
 
 $b->items();
 
 $b->add_items();
 
-$b->add_items([5,1,1], [6,1,1]);
-$b->add_items([5,1,1], [6,1,1]);
-$b->add_items([5,1,1], [6,1,1]);
-$b->add_items([5,1,1], [6,1,1]);
-$b->add_items([5,1,1], [6,1,1]);
-$b->add_items([5,1,1], [6,1,1]);
+$b->add_items([5,1,2], [6,3,4]);
 
 $b->items();
+warn "alive";
+my $clone = $b->invariant_clone;
+$clone->dump;
+warn "alive2";
+warn $clone->nitems;
+warn Dumper $clone->items;
+exit;
