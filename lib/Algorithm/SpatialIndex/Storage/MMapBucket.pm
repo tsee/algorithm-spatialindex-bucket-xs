e88eba2eb5698d7a421fcfe93effefd49a26c682
Steffen Mueller <smueller@cpan.org>
2011-07-10 20:24:57 +0200

    Implement dumping of nodes to disk as JSON
    This makes for an almost-complete (no options) persistent
    spatial index using mmap for the largest part of the memory
    requirements (I hope). Proof-of-principle quality, of course.

--- lib/Algorithm/SpatialIndex/Storage/MMapBucket.pm
+++ lib/Algorithm/SpatialIndex/Storage/MMapBucket.pm
@@ -17,8 +17,9 @@ use Data::Dumper;
 use JSON::XS ();
 
 use constant {
-  BUCKETS_FILE => '%s/buckets.mmap',
-  BUCKETS_INDEX_FILE => '%s/buckets_index.json',
+  NODES_FILE          => '%s/nodes.json',
+  BUCKETS_FILE        => '%s/buckets.mmap',
+  BUCKETS_INDEX_FILE  => '%s/buckets_index.json',
 };
 
 sub init {
@@ -31,10 +32,16 @@ sub init {
   }
 
   if ($opt->{load_mmap}) {
-    # TODO this is all nice and well for buckets. Or so I assume, but I can't know
-    # because this was never tested -- I can only dump (XS) buckets to disk at this point,
-    # not the rest of the tree, particularly not the nodes. Asks for A::SI::Node::XS. *sigh*
+    # FIXME do this properly
     my $dir = $opt->{path};
+
+    # Load the nodes from JSON
+    my $nodes_file = sprintf(NODES_FILE(), $dir);
+    my $unblessed_nodes_ary = JSON::XS::decode_json(do {local $/; open my $fh, "<", $nodes_file or die $!; <$fh>});
+    my $main_nodes_ary = $self->{nodes};
+    push @{$main_nodes_ary}, bless($_ => 'Algorithm::SpatialIndex::Node') for @$unblessed_nodes_ary;
+
+    # Load the buckets via mmap
     my $b_file = sprintf(BUCKETS_FILE(), $dir);
     my $b_index_file = sprintf(BUCKETS_INDEX_FILE(), $dir);
     croak("Can't locate buckets index file '$b_index_file'") if not -f $b_index_file;
@@ -55,12 +62,18 @@ sub init {
   }
 }
 
-sub write_buckets_to_disk {
+sub write_to_disk {
+  my $self = shift;
+  $self->_write_nodes_to_disk;
+  $self->_write_buckets_to_disk;
+}
+
+sub _write_buckets_to_disk {
   my $self = shift;
 
   my $dir = $self->{opt}{path};
   open my $buckets_fh, '>', sprintf(BUCKETS_FILE, $dir)
-      or die "Failed to open buckets file for writing: $!";
+    or die "Failed to open buckets file for writing: $!";
   binmode $buckets_fh;
 
   my $buckets_index = [];
@@ -81,6 +94,23 @@ sub write_buckets_to_disk {
 
   return $buckets_index;
 }
+
+sub _write_nodes_to_disk {
+  my $self = shift;
+
+  my $dir = $self->{opt}{path};
+  open my $nodes_fh, '>', sprintf(NODES_FILE, $dir)
+    or die "Failed to open nodes file for writing: $!";
+  binmode $nodes_fh;
+
+  # FIXME assumes the nodes are simple hashref objects!
+  print $nodes_fh JSON::XS::encode_json(
+    [map { +{%$_} } @{$self->{nodes}}] # poor man's un-blessing shallow clone
+  );
+
+  close $nodes_fh;
+}
+
 
 
 1;
