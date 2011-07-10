32b9024610f1133bd52a72c5f03917a354d7ddcf
Steffen Mueller <smueller@cpan.org>
2011-07-10 21:51:00 +0200

    use XSBucketTest for bench

--- xt/bench.pl
+++ xt/bench.pl
@@ -5,45 +5,46 @@ use Test::More;
 use Algorithm::SpatialIndex;
 use Algorithm::SpatialIndex::Strategy::QuadTree;
 use Time::HiRes qw(sleep time);
+use lib 't/lib';
+use Algorithm::SpatialIndex::XSBucketTest;
 
-my $start = time;
-my @limits = qw(12 -2 15 7);
-my $index = Algorithm::SpatialIndex->new(
-  bucket_class => $ENV{PERL_ASI_BUCKET_CLASS}||'Algorithm::SpatialIndex::Bucket::XS',
+
+mkdir 'tmpdir';
+my $tmpdir = 'tmpdir';
+use Algorithm::SpatialIndex::XSBucketTest;
+my $index = Algorithm::SpatialIndex::XSBucketTest->run('MMapBucket', path => $tmpdir);
+$index->storage->write_to_disk;
+
+
+my $index2 = Algorithm::SpatialIndex->new(
+  bucket_class => 'Algorithm::SpatialIndex::Bucket::XS',
   strategy => 'QuadTree',
-  storage  => 'Memory',
-  limit_x_low => $limits[0],
-  limit_y_low => $limits[1],
-  limit_x_up  => $limits[2],
-  limit_y_up  => $limits[3],
-  bucket_size => 2000,
+  storage  => 'MMapBucket',
+  limit_x_low => $index->limit_x_low,
+  limit_y_low => $index->limit_y_low,
+  limit_x_up  => $index->limit_x_up,
+  limit_y_up  => $index->limit_y_up,
+  bucket_size => $index->bucket_size,
+  path => $tmpdir,
+  load_mmap => 1,
 );
 
-isa_ok($index, 'Algorithm::SpatialIndex');
+Algorithm::SpatialIndex::XSBucketTest->run('MMapBucket', $index2);
 
-my $strategy = $index->strategy;
-isa_ok($strategy, 'Algorithm::SpatialIndex::Strategy::QuadTree');
+test_fetches($index, 'Original index');
+test_fetches($index2, 'Mmap index');
 
-is($strategy->no_of_subnodes, 4, 'QuadTree has four subnodes');
-is_deeply([$strategy->coord_types], [qw(double double double double double double)], 'QuadTree has six coordinates');
-
-#my $scale = 1;
-my $scale = 70;
-my $item_id = 0;
-foreach my $x (map {$_/$scale} $limits[0]*$scale..$limits[2]*$scale) {
-  foreach my $y (map {$_/$scale} $limits[1]*$scale..$limits[3]*$scale) {
-    $index->insert($item_id++, $x, $y);
+sub test_fetches {
+  my $idx = shift;
+  my $name = shift;
+  my $time = time;
+  my $n = 5000;
+  for my $i (1..$n) {
+    my @items = $idx->get_items_in_rect(qw( 13 0 13.2 0.2 ));
+    warn scalar(@items) if $i == 1;
   }
+  printf "$name: Each get took: %.3f ms\n", (time()-$time)/$n*1000;
 }
 
-diag("Inserted $item_id items");
-printf "Filling took %.3f\n", time()-$start;
-
-my $time = time;
-my $n = 500;
-for my $i (1..$n) {
-  my @items = $index->get_items_in_rect(qw( 13 0 13.2 0.2 ));
-  warn scalar(@items) if $i == 1;
-}
-printf "Each get took: %.3f ms\n", (time()-$time)/$n*1000;
+done_testing;
 
